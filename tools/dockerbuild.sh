22c5a4f3cfcd37eb1b8671287de42f004b9ef15e
jefeweisen <jefeweisen@users.noreply.github.com>
2015-07-16 14:42:31 -0700

    Since vagrant share state only changes at 'vagrant up' time, so should $CHVDOCKER_YAML

--- tools/dockerbuild.sh
+++ tools/dockerbuild.sh
@@ -8,6 +8,12 @@ cp config/application.conf src/main/resources/application.conf
 sbt assembly
 rm -rf "$adirRepo/dockerbuild/dockerpipeline/script"
 cp -r "$adirRepo/script" "$adirRepo/dockerbuild/dockerpipeline/script"
+
+# or should we decoding git core.eol and core.autocrlf?
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "win32" ]]
+then
+  dos2unix "$adirRepo/dockerbuild/dockerpipeline/script/*"
+fi
 # Spark binaries (e.g. spark-submit) install disabled.  For now, we're 
 # doing only java-main style spark, which can all come from maven.
 #bash "$adirRepo/tools/install_spark.sh"
